53c01efe88ce5906d9ceef4c8cf6f647ca0f107f
Dustin Specker <DustinSpecker@DustinSpecker.com>
2023-03-29 17:49:59 -0500

    integration: remove GinkgoT usage

--- integration/discordnotifyipchange_test.go
+++ integration/discordnotifyipchange_test.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	"github.com/onsi/gomega/ghttp"
@@ -14,19 +14,15 @@ import (
 
 var _ = Describe("discord-notify-ip-change", func() {
 	It("sends message to Discord webhook", func() {
-		g := gomega.NewWithT(GinkgoT())
-
 		discordNotifyIPChangeCLI, err := gexec.Build("github.com/dustinspecker/discord-notify-ip-change/cmd/discord-notify-ip-change")
-		g.Expect(err).To(gomega.BeNil(), "failed to build discord-notify-ip-change")
-
-		ghttptest := ghttp.NewGHTTPWithGomega(g)
+		Expect(err).To(BeNil(), "failed to build discord-notify-ip-change")
 
 		ipServer := ghttp.NewServer()
 		defer ipServer.Close()
 
 		ipServer.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttptest.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
+				ghttp.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
 			),
 		)
 
@@ -35,103 +31,91 @@ var _ = Describe("discord-notify-ip-change", func() {
 
 		discordServer.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttptest.VerifyContentType("application/json"),
-				ghttptest.VerifyRequest(http.MethodPost, "/"),
-				ghttptest.VerifyBody([]byte(`{"content": "192.168.0.1"}`)),
+				ghttp.VerifyContentType("application/json"),
+				ghttp.VerifyRequest(http.MethodPost, "/"),
+				ghttp.VerifyBody([]byte(`{"content": "192.168.0.1"}`)),
 			),
 		)
 
 		command := exec.Command(discordNotifyIPChangeCLI, "-ip-url", ipServer.URL(), "-discord-webhook-url", discordServer.URL(), "-timeout", "5s")
 		session, err := gexec.Start(command, os.Stdout, os.Stderr)
-		g.Expect(err).To(gomega.BeNil(), "error while running command")
+		Expect(err).To(BeNil(), "error while running command")
 
-		g.Eventually(session).Should(gexec.Exit(0), "command should exit with 0 return code")
+		Eventually(session).Should(gexec.Exit(0), "command should exit with 0 return code")
 
-		g.Expect(ipServer.ReceivedRequests()).Should(gomega.HaveLen(1), "expected only 1 request made to server")
-		g.Expect(discordServer.ReceivedRequests()).Should(gomega.HaveLen(1), "expected only 1 message to be sent to discord")
+		Expect(ipServer.ReceivedRequests()).Should(HaveLen(1), "expected only 1 request made to server")
+		Expect(discordServer.ReceivedRequests()).Should(HaveLen(1), "expected only 1 message to be sent to discord")
 	})
 
 	It("returns error when unable to get IP", func() {
-		g := gomega.NewWithT(GinkgoT())
-
 		discordNotifyIPChangeCLI, err := gexec.Build("github.com/dustinspecker/discord-notify-ip-change/cmd/discord-notify-ip-change")
-		g.Expect(err).To(gomega.BeNil(), "failed to build discord-notify-ip-change")
+		Expect(err).To(BeNil(), "failed to build discord-notify-ip-change")
 
 		command := exec.Command(discordNotifyIPChangeCLI, "-ip-url", "", "-timeout", "5s")
 		session, err := gexec.Start(command, os.Stdout, os.Stderr)
-		g.Expect(err).To(gomega.BeNil(), "error while running command")
+		Expect(err).To(BeNil(), "error while running command")
 
-		g.Eventually(session).Should(gexec.Exit(1), "command should exit with non-zero return code")
+		Eventually(session).Should(gexec.Exit(1), "command should exit with non-zero return code")
 
-		g.Expect(session.Err).To(gbytes.Say(`.*error getting public IP: error getting URL.*`), "should print error")
+		Expect(session.Err).To(gbytes.Say(`.*error getting public IP: error getting URL.*`), "should print error")
 	})
 
 	It("returns error when unable to send message", func() {
-		g := gomega.NewWithT(GinkgoT())
-
 		discordNotifyIPChangeCLI, err := gexec.Build("github.com/dustinspecker/discord-notify-ip-change/cmd/discord-notify-ip-change")
-		g.Expect(err).To(gomega.BeNil(), "failed to build discord-notify-ip-change")
-
-		ghttptest := ghttp.NewGHTTPWithGomega(g)
+		Expect(err).To(BeNil(), "failed to build discord-notify-ip-change")
 
 		ipServer := ghttp.NewServer()
 		defer ipServer.Close()
 
 		ipServer.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttptest.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
+				ghttp.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
 			),
 		)
 
 		command := exec.Command(discordNotifyIPChangeCLI, "-ip-url", ipServer.URL(), "-discord-webhook-url", "", "-timeout", "5s")
 		session, err := gexec.Start(command, os.Stdout, os.Stderr)
-		g.Expect(err).To(gomega.BeNil(), "error while running command")
+		Expect(err).To(BeNil(), "error while running command")
 
-		g.Eventually(session).Should(gexec.Exit(1), "command should exit with 1 return code")
-		g.Expect(session.Err).To(gbytes.Say(`.*error sending message to discord: error sending message: Post "": unsupported protocol scheme "".*`), "should print error")
+		Eventually(session).Should(gexec.Exit(1), "command should exit with 1 return code")
+		Expect(session.Err).To(gbytes.Say(`.*error sending message to discord: error sending message: Post "": unsupported protocol scheme "".*`), "should print error")
 
-		g.Expect(ipServer.ReceivedRequests()).Should(gomega.HaveLen(1), "expected only 1 request made to server")
+		Expect(ipServer.ReceivedRequests()).Should(HaveLen(1), "expected only 1 request made to server")
 	})
 
 	It("returns error when unable to render message", func() {
-		g := gomega.NewWithT(GinkgoT())
-
 		discordNotifyIPChangeCLI, err := gexec.Build("github.com/dustinspecker/discord-notify-ip-change/cmd/discord-notify-ip-change")
-		g.Expect(err).To(gomega.BeNil(), "failed to build discord-notify-ip-change")
-
-		ghttptest := ghttp.NewGHTTPWithGomega(g)
+		Expect(err).To(BeNil(), "failed to build discord-notify-ip-change")
 
 		ipServer := ghttp.NewServer()
 		defer ipServer.Close()
 
 		ipServer.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttptest.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
+				ghttp.RespondWith(http.StatusOK, `{"ip": "192.168.0.1"}"`),
 			),
 		)
 
 		command := exec.Command(discordNotifyIPChangeCLI, "-ip-url", ipServer.URL(), "-discord-webhook-url", "", "-timeout", "5s", "-format", "{{ .}")
 		session, err := gexec.Start(command, os.Stdout, os.Stderr)
-		g.Expect(err).To(gomega.BeNil(), "error while running command")
+		Expect(err).To(BeNil(), "error while running command")
 
-		g.Eventually(session).Should(gexec.Exit(1), "command should exit with 1 return code")
-		g.Expect(session.Err).To(gbytes.Say(`.*error rendering message: error parsing template: template: message:1: bad character.*`), "should print error")
+		Eventually(session).Should(gexec.Exit(1), "command should exit with 1 return code")
+		Expect(session.Err).To(gbytes.Say(`.*error rendering message: error parsing template: template: message:1: bad character.*`), "should print error")
 
-		g.Expect(ipServer.ReceivedRequests()).Should(gomega.HaveLen(1), "expected only 1 request made to server")
+		Expect(ipServer.ReceivedRequests()).Should(HaveLen(1), "expected only 1 request made to server")
 	})
 
 	It("returns error when unable to parse timeout", func() {
-		g := gomega.NewWithT(GinkgoT())
-
 		discordNotifyIPChangeCLI, err := gexec.Build("github.com/dustinspecker/discord-notify-ip-change/cmd/discord-notify-ip-change")
-		g.Expect(err).To(gomega.BeNil(), "failed to build discord-notify-ip-change")
+		Expect(err).To(BeNil(), "failed to build discord-notify-ip-change")
 
 		command := exec.Command(discordNotifyIPChangeCLI, "-ip-url", "", "-timeout", "10f")
 		session, err := gexec.Start(command, os.Stdout, os.Stderr)
-		g.Expect(err).To(gomega.BeNil(), "error while running command")
+		Expect(err).To(BeNil(), "error while running command")
 
-		g.Eventually(session).Should(gexec.Exit(1), "command should exit with non-zero return code")
+		Eventually(session).Should(gexec.Exit(1), "command should exit with non-zero return code")
 
-		g.Expect(session.Err).To(gbytes.Say(`.*unable to parse timeout: time: unknown unit "f" in duration "10f".*`), "should print error")
+		Expect(session.Err).To(gbytes.Say(`.*unable to parse timeout: time: unknown unit "f" in duration "10f".*`), "should print error")
 	})
 })
